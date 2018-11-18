a38329958b54fe4d8827ef95631b36df8d673a05
iteras <imreteras@gmail.com>
2018-11-18 17:22:58 +0200

    Added clean and solid django_server project with metrics app. First tests done with manipulating with SqlLite3 through Django projcet

---
+++ Server/django_server/metrics/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+import json
+
+# Create your models here.
+class SBC(models.Model):
+    temperature = models.FloatField(default=0)
+    ts = models.DateTimeField()
+
+    def __str__(self):
+        data = {#'ts' : self.ts,
+             'temperature': self.temperature}
+        s = json.dumps(data)
+        return s
+
+class Room_environment(models.Model):
+    sbc = models.ForeignKey(SBC,on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    room = models.CharField(max_length=16)
+    temperature = models.FloatField(default=-1)
+    humidity = models.FloatField(default=-1)
+    ts = models.DateTimeField()
+
+    def __str__(self):
+        data = {'address': self.address,
+                'room': self.address,
+                'temperature': self.temperature,
+#                'ts': self.ts,
+                'humidity': self.humidity}
+        s = json.dumps(data)
+        return s

9332afa140d61974535bd45109264a4b35e1f14a
iteras <imre.teras@starship.co>
2019-01-16 14:15:32 +0200

    Added shuddown button software for GPIO pin 3. Needs initial testing.

--- src/shut_down_GPIO.py
+++ src/shut_down_GPIO.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
+import subprocess
+import time
 
 if not os.getegid() == 0:
     sys.exit('Script must be run as root')
@@ -10,62 +12,41 @@ from pyA20.gpio import gpio
 from pyA20.gpio import port
 
 # Declare
-button = port.PA3
-
+shutdown_button = port.PA3
 
 # Init GPIO and declare outputs and inputs
 gpio.init()
-gpio.setcfg(button, gpio.INPUT)
+gpio.setcfg(shutdown_button, gpio.INPUT)
+
 
 class button_controller(object):
 
     class __button_controller:
         def __init__(self):
-            self.state = None
+            self.button_pushed = None
 
         def __str__(self):
             return repr(self) + self.state
 
         def begin(self):
             try:
-                if self.state is not None:
-                    print("STATE CHANGED TO : %s" % self.current_color)
-                    color = decide_color(self.current_color)
-                    differences = list(set(all_colors) - set(color))
-                    for led in color:
-                        gpio.output(led, 1)
-                    for led in differences:
-                        gpio.output(led, 0)
+                while 1:
+                    if gpio.input(shutdown_button) == 1:
+                        print("Shutdown button pressed : %s" % self.button_pushed)
+#                       subprocess.call("/sbin/shutdown now", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        time.sleep(2000)
             except KeyboardInterrupt:
-                # All LEDs off
-                for led in white:
-                    gpio.output(led, 0)
                 print("Goodbye.")
 
     instance = None
 
     def __new__(cls):  # __new__ always a classmethod
-        if not LED_controller.instance:
-            LED_controller.instance = LED_controller.__LED_controller()
-        return LED_controller.instance
+        if not button_controller.instance:
+            button_controller.instance = button_controller.__button_controller()
+        return button_controller.instance
 
     def __getattr__(self, name):
         return getattr(self.instance, name)
 
     def __setattr__(self, name):
         return setattr(self.instance, name)
-
-
-def decide_color(state):
-    if state == "red":
-        return red
-    elif state == "green":
-        return green
-    elif state == "blue":
-        return blue
-    elif state == "yellow":
-        return yellow
-    elif state == "magneta":
-        return magneta
-    elif state == "white":
-        return white

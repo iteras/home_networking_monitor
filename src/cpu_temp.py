2fb5af9bb1c782b840468c549966c05c51543a12
iteras <imre.teras@starship.co>
2019-01-16 13:20:10 +0200

    Cleaned and formatted the code to cleaner look.

--- src/cpu_temp.py
+++ src/cpu_temp.py
@@ -8,12 +8,12 @@ import time
 if not os.getegid() == 0:
     sys.exit('Script must be run as root')
 
+
 def get_cpu_temp():
     temp = float(subprocess.check_output(["cat", "/sys/devices/virtual/thermal/thermal_zone0/temp"])[:-2]
                  .decode("UTF-8"))/100
     return temp
 
-print(get_cpu_temp())
 
 if __name__ == "__main__":
     from pyA20.gpio import gpio

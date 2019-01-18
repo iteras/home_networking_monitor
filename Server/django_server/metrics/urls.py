b645214ea723ed38dff0d43a1c6e8ee38efc79aa
iteras <imre.teras@starship.co>
2019-01-18 09:36:50 +0200

    Data from DB is shown on first url path. Implemented also first primial view and template.

--- Server/django_server/metrics/urls.py
+++ Server/django_server/metrics/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:sbc_id>',views.sbc_temperature,name="detail")
+    path('<int:sbc_id>', views.sbc_temperature, name="sbc_temperature"),
+    path('<int:sbc_id>/data', views.sbc_temperature_data, name="sbc_temperature_data")
 ]
--- Server/django_server/metrics/views.py
+++ Server/django_server/metrics/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import SBC
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the metrics index.")
 
 
-def sbc_temperature(request,sbc_id):
+def sbc_temperature(request, sbc_id):
     response = "SBC: %s"
     return HttpResponse(response % sbc_id)
+
+
+def sbc_temperature_data(request, sbc_id):
+    SBC_list = SBC.objects.order_by("ts")
+#    output = "".join([str(s) for s in SBC_list])
+    template = loader.get_template('metrics/index.html')
+    context = {
+        'SBC_list': SBC_list,
+    }
+    return HttpResponse(template.render(context, request))

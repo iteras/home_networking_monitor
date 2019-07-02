a014c7211b334bb16a6458bf239a14365fffeb29
iteras <imreteras@gmail.com>
2019-07-02 21:41:18 +0300

    Implementing probe data transfer to server. Sending proper payload with Room_Environment parameters. Filling room parameter with 'outdoor'. Rose retry count for wifi failure to 60 attempts.

--- src/hum_temp_probe/hum_temp_probe.ino
+++ src/hum_temp_probe/hum_temp_probe.ino
@@ -4,8 +4,8 @@
 
 #define DHTTYPE DHT22
 #define DHTPIN 5
-DHT dht(DHTPIN,DHTTYPE);
-#define NB_TRYWIFI 10 // WIFI connection attempts before dumping it and going to sleep
+DHT dht(DHTPIN, DHTTYPE);
+#define NB_TRYWIFI 60 // WIFI connection attempts before dumping it and going to sleep
 
 int chk;
 float temp;
@@ -15,7 +15,7 @@ const char* ssid = "      "; //your WiFi Name
 const char* password = "52101ehhf16";  //Your Wifi Password
 void setup() {
   Serial.begin(115200);
-  delay(10); 
+  delay(10);
 
   // Connect to Wi-Fi
   int _try = 0;
@@ -23,7 +23,7 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     _try++;
-
+    Serial.println("Wifi not starting");
     if (_try >= NB_TRYWIFI) {
       ESP.deepSleep(60e6);
     }
@@ -37,21 +37,23 @@ void loop() {
   delay(2000);
   //Read data and store it to variables hum and temp
   hum = dht.readHumidity();
-  temp= dht.readTemperature();
-//  Serial.print("Humidity: ");Serial.print(hum);Serial.print(", temperature: ");Serial.println(temp);
-  String load = "{\"hum\":" +  String(hum) + ", \"temp\" : " + String(temp) + "}"; //TODO: Apply corred load
+  temp = dht.readTemperature();
+  //  Serial.print("Humidity: ");Serial.print(hum);Serial.print(", temperature: ");Serial.println(temp);
+  Serial.print(temp); Serial.print(" "); Serial.println(hum);
+  String load = "{\"humidity\":" +  String(hum) + ", \"temperature\" : " + String(temp) + ", " + "\"address\":" + "\"Pargi 1 - 16\", \"room\" : \"outdoor\"}";
   Serial.println(load);
-  if (not ((hum != hum) or (temp != temp))){
+  if (not ((hum != hum) or (temp != temp))) {
     HTTPClient http;
-    
-    http.begin("http://192.168.0.100:8080/metrics/"); //TODO: APPLY CORRECT URL
+
+    http.begin("http://192.168.0.100:8000/metrics/re_post");
     http.addHeader("Content-Type", "json/application");
     int httpCode = http.POST(load);
-    
+//    delay(5000);
+
     String payload = http.getString();
-    
+
     http.end();
   }
   ESP.deepSleep(60e6);
-  
- }
+
+}
